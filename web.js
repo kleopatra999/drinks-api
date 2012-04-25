11fbda87c88a0e340da7a3af1acc8b3237930239
Mark Stuart <stuartmark@gmail.com>
2012-04-24 20:38:12 -0500

    1st commit

--- web.js
+++ web.js
@@ -1,12 +1,50 @@
-var express = require('express');
+var express = require('express'),
+	app = express.createServer(express.logger());
 
-var app = express.createServer(express.logger());
+var bars = ['blueline', 'ciieast', 'chasers', 'daddios', 'drifters',
+			'elroys', 'fatjacks', 'firehouse', 'illinoisbrewingco', 'killarneys', 
+			'maggiemileys', 'maguires', 'mulligans', 'pubii', 'sixstrings', 'theloft'];
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/bars', function(req, res) {
+	res.sendfile('responses/bars.json');
 });
 
+function isBar(bar){
+	var found = false,
+		len = bars.length;
+		
+	while(len--){
+		console.log( 'bars[len]: ' + bars[len] + ' bars[len].match(bar): ' + bars[len].match(bar) );
+		if ( bars[len].match(bar) ){
+			found = true;
+			break;
+		}
+	}
+	return found;
+}
+
+function getBar(req, res){
+	var bar = req.params.bar;
+	bar = bar.replace(/\s/g, "");
+	bar = bar.toLowerCase();
+	return bar;
+}
+
+app.get('/bars/:bar', function(req, res) {
+	var bar = getBar(req, res);
+	console.log('bar parsed to: ' + bar );
+	if ( !isBar(bar) ){
+		console.log('bar not found');
+		res.send('{"status":"error", "message": ' + req.params.bar + '" was not found.  Please try again."}');
+	} else {
+		console.log('bar found');
+		res.sendfile('responses/' + bar + '.json');
+	}
+});
+
+// Start Server and listen on port 3000
 var port = process.env.PORT || 3000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
